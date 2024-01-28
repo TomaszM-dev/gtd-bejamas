3e04643a6297d3a340cfefcfa85fa19bc64ab57f
TomaszM-dev <tomekmalocha@gmail.com>
2024-01-28 03:28:15 +0100

    fixing responsiveness issue in hero section

--- components/home-page-ui/hero.tsx
+++ components/home-page-ui/hero.tsx
@@ -10,15 +10,15 @@ const Hero = () => {
     <main className="">
       <div className="relative">
         <Image
-          className="object-cover w-full max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[30vh] "
+          className="object-cover w-full max-lg:object-right-bottom max-lg:h-[40vh] max-md:h-[40vh] max-sm:h-[50vh] "
           alt="background-image"
           src={hero}
         ></Image>
-        <h1 className=" absolute top-[50%] left-[45%] translate-x-[-50%] translate-y-[-50%] whitespace-nowrap text-secondaryColor text-[6rem] font-[900]  max-lg:text-[5rem] max-md:left-[50%] max-md:text-[4.5rem] max-sm:text-[2.8rem]">
+        <h1 className=" absolute top-[50%] left-[45%] translate-x-[-50%] translate-y-[-50%] whitespace-nowrap text-secondaryColor text-[6rem] font-[900]  max-lg:text-[5rem] max-lg:left-10 max-lg:translate-x-0 max-md:text-[4.5rem] max-sm:left-4 max-sm:top-28   max-sm:text-[3rem]">
           Hello, we're gtd.
         </h1>
       </div>
-      <div className="flex gap-28 max-lg:gap-14 max-lg:mt-[-4rem] w-full justify-center max-sm:flex-col max-sm:items-center max-sm:gap-18 mt-[-6rem] max-md:gap-8 z-10 relative max-sm:mt-[-3rem]">
+      <div className="flex gap-28 max-lg:gap-14 max-lg:mt-[-4rem] w-full justify-center max-sm:flex-col max-sm:items-center max-sm:gap-18 mt-[-6rem] max-md:gap-8 z-10 relative max-sm:mt-[2rem]">
         <Image
           className=" max-lg:w-40 max-lg:h-40 max-md:w-30 max-md:h-30 max-sm:w-44 max-sm:h-44 "
           src={teamLisa}
